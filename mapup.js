6bd5548eaa749386045c4818d6e8e92c401733f6
Ashok Kumar <68746723+ashok57@users.noreply.github.com>
2023-12-11 12:45:36 +0530

    Add files via upload

---
+++ mapup.js
@@ -0,0 +1,14 @@
+// // Add a scroll event listener
+// window.addEventListener('scroll', function() {
+//     // Get the header element
+//     var header = document.querySelector('.sticky-header');
+
+//     // Check if the user has scrolled down
+//     if (window.scrollY > 0) {
+//         // If yes, add a class to the header to modify its style
+//         header.classList.add('scrolled');
+//     } else {
+//         // If no, remove the class
+//         header.classList.remove('scrolled');
+//     }
+// });
